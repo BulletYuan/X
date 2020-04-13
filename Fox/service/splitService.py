caf76da0d10add377d5ccea7d6dee6747766c918
BulletYuan <326583424@qq.com>
2020-04-13 16:38:29 +0800

    modify Fox Py

--- Fox/service/splitService.py
+++ Fox/service/splitService.py
@@ -25,10 +25,34 @@ def split_data(data=[]):
     content = ""
     for item in data:
         # i_content = item["content"].replace('^\S+', '')
-        i_content = re.sub(r'\n|\r|\s', '', item['content'])
-        print i_content
-        content += " /".join(jieba.cut(i_content))
+        i_content = re.sub(r'\n|\r|\s|\t', '', item['content'])
+        split_content = list(jieba.cut(i_content))
+        content += " ".join(split_content)
+        # print split_content
+        count = top_count_data(split_content)
     return content
+
+
+def top_count_data(data=[]):
+    if len(data) == 0:
+        return []
+    items = []
+
+    def find_index(self, i, value):
+        return sorted(self, key=lambda x: x[i] != value)[0]
+
+    for item in data:
+        try:
+            result=find_index(data, 0, item)
+            print result
+            if find_index(data, 0, item):
+                pass
+        except IOError:
+            el = (item, data.count(item))
+            items.append(el)
+    items.sort(key=lambda x: x[1], reverse=True)
+    print items
+    return items
 
 
 def split():
