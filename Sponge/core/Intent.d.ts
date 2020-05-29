5249aa533c91308b1cd8605b707864fe8f3bdd72
BulletYuan <326583424@qq.com>
2020-05-29 12:17:15 +0800

    remove Cache interface & modify Intent methods

--- Sponge/core/Intent.d.ts
+++ Sponge/core/Intent.d.ts
@@ -9,8 +9,8 @@ declare class Sponge {
     private Storage: object;
 
     private ObjectStringify(obj: any, append?: StringifyAppend): string;
-    public Commit(key: string | Array<string>, state: string | boolean | object | any): void;
-    public Pull(key: string, seccess?: Function, error?: Function, complate?: Function): void;
+    public Publish(key: string | Array<string>, state: string | boolean | object | any): void;
+    public Subscribe(key: string, seccess?: Function, error?: Function, complate?: Function): void;
 }
 
 export default Sponge
--- Sponge/index.js
+++ Sponge/index.js
@@ -1,12 +1,12 @@
 let Intent, Cache;
 if (typeof require !== 'undefined') {
   Intent = require('./core/Intent');
-  Cache = require('./core/Cache');
+  // Cache = require('./core/Cache');
 }
 
 if (typeof module !== 'undefined') {
   module.exports = {
     Intent,
-    Cache,
+    // Cache,
   };
 }
